7c689bf0ec5398285dd8b107942a6d27d067f1f2
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-19 00:06:51 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,7 +1,12 @@
 class SecretsController < ApplicationController
   before_action :require_login
-  
+
   def show
     @secret = Secret.find(params[:id])
   end
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include?:user_id
 end
